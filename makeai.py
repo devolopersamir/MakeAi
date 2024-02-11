add0f5a5294ebfcb9a7947363848bd955691d053
Devoloper_Samir <154772869+devolopersamir@users.noreply.github.com>
2024-02-11 18:12:06 +0600

    Make Ai Python File

---
+++ makeai.py
@@ -0,0 +1,72 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime 
+
+listener = sr.Recognizer()
+bindu = pyttsx3.init()
+
+def talk(text):
+    bindu.say(text)
+    bindurunAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'bindu' in command:
+                command = command   import speech_recognition as sr
+import pyttsx3
+import datetime
+import pywhatkit
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+alexa = pyttsx3.init()
+voices = alexa.getProperty('voices')
+alexa.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    alexa.say(text)
+    alexa.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    if 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+
+
+        talk('Current time is ' + time)replace('bindu', '')
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    if 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+        
